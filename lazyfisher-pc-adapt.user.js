8c2d80511c4c920127d9428597a6a4fa28ba5551
yf96 <136201390+yf96@users.noreply.github.com>
2026-08-05 01:58:32 +0800

    v1.3.0

--- lazyfisher-pc-adapt.user.js
+++ lazyfisher-pc-adapt.user.js
@@ -2,7 +2,7 @@
 // @name         LazyFisher PC Adapt
 // @namespace    https://lazyfisher.toogle.club/
 // @version      1.3.0
-// @description  Mouse wheel horizontal scroll, drag scroll, text word-wrap for LazyFisher
+// @description  Horizontal scroll (wheel), drag scroll, text wrap for LazyFisher
 // @author       yf96
 // @match        https://lazyfisher.toogle.club/*
 // @grant        none
