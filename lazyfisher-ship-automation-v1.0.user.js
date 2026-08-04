9c073f46b3e0ce46ae978476bf3d1b4ff7eafa95
yf96 <136201390+yf96@users.noreply.github.com>
2026-08-05 01:51:17 +0800

    v1.0.0: description update

--- lazyfisher-ship-automation-v1.0.user.js
+++ lazyfisher-ship-automation-v1.0.user.js
@@ -2,7 +2,7 @@
 // @name         LazyFisher Ship Ops
 // @namespace    https://lazyfisher.toogle.club
 // @version      1.0.0
-// @description  Ship automation with target-fish-driven loop
+// @description  Ship automation + target fish loop for LazyFisher
 // @author       yf96
 // @match        https://lazyfisher.toogle.club/*
 // @icon         https://lazyfisher.toogle.club/pwa/fish.svg
